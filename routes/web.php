cb4d21e4e0c034555fafccc0f8aec12f159b462f
soru <ityouna007@gmail.com>
2024-12-17 06:30:27 +0900

    add some comments & changed the order

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
+// homeページはログインしているユーザー専用です
+// ログインしていないユーザーがアクセスした場合はログインページにリダイレクトします
+// middlewareメソッドを使って、homeページにアクセスする前に認証を行います
 Route::get('/home', [HomeController::class, 'home'])->name('home')->middleware('auth');
+
+Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('signup');
+Route::post('/signup', [SignupController::class, 'signup']);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
-
-Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('signup');
-Route::post('/signup', [SignupController::class, 'signup']);
