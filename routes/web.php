7d3543906f74e2eb83e890742f198705489281ef
soru <ityouna007@gmail.com>
2024-12-22 02:17:00 +0900

    Change the order

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ Route::get('/', function () {
 // middlewareメソッドを使って、homeページにアクセスする前に認証を行います
 Route::get('/home', [HomeController::class, 'home'])->name('home')->middleware('auth');
 
-Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('signup');
-Route::post('/signup', [SignupController::class, 'signup']);
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('show-login');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('show-signup');
+Route::post('/signup', [SignupController::class, 'signup'])->name('signup');
