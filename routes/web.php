db8cd3b7353c4dd4d4cc59c45b45936b8ddde289
soru <ityouna007@gmail.com>
2024-12-27 22:59:01 +0900

    ✨ 認証されたユーザー向けにカテゴリ作成機能を追加し、ルーティングを整理

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,26 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\SignupController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
-// homeページはログインしているユーザー専用です
-// ログインしていないユーザーがアクセスした場合はログインページにリダイレクトします
-// middlewareメソッドを使って、homeページにアクセスする前に認証を行います
-Route::get('/home', [HomeController::class, 'home'])->name('home')->middleware('auth');
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('show-login');
-Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('show-signup');
-Route::post('/signup', [SignupController::class, 'signup'])->name('signup');
+Route::get('/signup', [SignupController::class, 'showRegistrationForm'])->name('signup');
+Route::post('/signup', [SignupController::class, 'signup']);
+
+Route::get('/home', [HomeController::class, 'home'])->name('home')->middleware('auth');
+
+Route::get('/home/create/category', [QuestionController::class, 'showCreateCategoryForm'])->name('create-category')->middleware('auth');
+Route::post('/home/create/category', [QuestionController::class, 'createCategory'])->middleware('auth');
+
+Route::get('/home/categories/{category_id}/questions', [QuestionController::class, 'getQuestions'])->name('get-questions')->middleware('auth');
